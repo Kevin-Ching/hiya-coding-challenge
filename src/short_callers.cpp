20194d13742685fb6d59fbcda29af5491516fb5e
Kevin Ching <keching@berkeley.edu>
2024-03-29 05:44:33 -0700

    Changed a comment

--- src/short_callers.cpp
+++ src/short_callers.cpp
@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
                 return input_error();
             }
 
-            // Check timestamp
+            // Make sure timestamps are non-decreasing
             if (event.timestamp < last_timestamp) {
                 cout << "Not time-ordered error. Timestamp " << event.timestamp << " appears after " << last_timestamp  << "." << endl;
                 return 1;
